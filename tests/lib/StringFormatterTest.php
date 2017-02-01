8e8f56df70d545a46e2e5e92aab4db0b06c963b1
HitmoSingh <mohit93700@gmail.com>
2017-02-01 12:57:52 +0100

    update and add test functions - total 5

--- tests/lib/StringFormatterTest.php
+++ tests/lib/StringFormatterTest.php
@@ -6,29 +6,40 @@ use Lib\StringFormatter;
 
 class StringFormatterTest extends \PHPUnit_Framework_TestCase
 {
-	public function testConcatStringSuccess()
-	{
-		$myString = new StringFormatter();
-		$result = $myString->concatString("allo", "mora");
+	public function testConcatStringSuccess(){
 
-		$this->assertEquals("allomora",$result);
+		$myString = new StringFormatter();
+		$result = $myString->concatString("camel", "case");
+		$this->assertEquals("camelcase",$result);
 	}
 
-	public function testConcatStringError()
-	{
+	public function testConcatStringError(){
 		/*
-		$myClass = new MyClass();
-        $result = $myClass->IsBlue('blue');
+			$myClass = new MyClass();
+	        $result = $myClass->IsBlue('blue');
 
-        $this->assert_False($result);
+	        $this->assert_False($result);
         */
 	}
 
-	public function testToCamelCase()
-	{
-		$myString = new StringFormatter();
-		$result = $myString->toCamelCase("allo", "mora");
+	public function testToCamelCase(){
 
-		$this->assertEquals("alloMora", $result);
+		$myString = new StringFormatter();
+		$result = $myString->toCamelCase("camel", "case");
+		$this->assertEquals("camelCase", $result);
 	}
+
+	public function testPrefixWithFalseParameterSuccess(){
+
+        $myString = new StringFormatter();
+        $result = $myString->prefix("camel", "case", false);
+        $this->assertEquals("camelcase", $result);
+    }
+
+   public function testPrefixWithTrueParameterSuccess(){
+
+        $myString = new StringFormatter();
+        $result = $myString->prefix("camel", "case", true);
+        $this->assertEquals("camelCase", $result);
+    }
 }
